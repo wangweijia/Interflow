7d04068e32d0f95ff82fd9964bbf5a10bea78e58
王唯佳 <wwjwangweijia@163.com>
2019-02-26 18:04:19 +0800

    web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css';    
+import Websocket from 'react-websocket';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            messageList: [],
+            inputText: ''
+        }
+
+        this.isConnented = false;
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Websocket ref={(view)=>{
+            this.ws = view;
+        }} url='ws://127.0.0.1:8080/'
+              onMessage={(data)=>{
+                // let result = JSON.parse(data);
+                console.log(data);
+                // this.setState({count: this.state.count + result.movement});
+                let newList = this.state.messageList;
+                newList.push(data);
+                this.setState({
+                  messageList: newList
+                });
+              }} onOpen={()=>{
+                  this.isConnented = true;
+              }} />
+
+          <div style={{
+            flexDirection: 'column'
+          }} >  
+              input view
+              <input type={'text'} value={this.state.inputText} onChange={(event)=>{
+                this.setState({
+                  inputText: event.target.value
+                })
+              }} />
+              <button onClick={()=>{
+                if (this.ws) {
+                  this.ws.sendMessage(this.state.inputText);
+                  this.setState({
+                    inputText: ''
+                  })
+                }
+              }} >send</button>
+          </div>
+          <div>
+            {this.state.messageList.map((item, index)=>{
+              return <div key={index} >{item}</div>
+            })}
+          </div>
       </div>
     );
   }
